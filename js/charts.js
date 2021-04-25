a74a5980f892001a05cba4291d1aa30d5d0fbcff
erinrkahn <77405273+erinrkahn@users.noreply.github.com>
2021-04-25 16:36:15 -0700

    add OTU chart

--- js/charts.js
+++ js/charts.js
@@ -60,7 +60,7 @@ function buildCharts(sample) {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = data.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var firstResult = resultArray[0];
 
@@ -69,23 +69,29 @@ function buildCharts(sample) {
 
     var otuLabels = firstResult.otu_labels;
 
-    var sampleValues = firstResult.sample_values;
+    var sampleValues = firstResult.sample_values.slice(0, 10).reverse();
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = otuIds.slice(0, 10).map(otu => "OTU " + otu).reverse()
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
+    var barData = [{
+      x: sampleValues,
+      y: yticks,
+      type: "bar",
+      orientation: 'h'
+    }   
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis: { title: "Number of Bacteria" },
+      //yaxis: { title: ""}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
